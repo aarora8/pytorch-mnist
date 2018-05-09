dbf0976ce63325f329e462a1accb907f4134a52f
aarora8 <aarora8@jhu.edu>
2018-05-09 15:45:49 -0400

    adding bash and perl files in run.sh

--- run.sh
+++ run.sh
@@ -2,20 +2,31 @@
 
 set -e # exit on error
 echo "$0 $@"
+
+
+. ./cmd.sh
 . ./parse_options.sh
 
-stage=0
+stage=1
 
-/home/aaror/yes/bin/python ./mnist_cuda.py \
-    --epochs 2 --no-cuda
+if [ $stage -le 0 ]; then
+  /home/aaror/yes/bin/python ./mnist_cuda.py \
+      --epochs 2 --no-cuda
+  exit 0;
+fi 
 
-#CUDA_VISIBLE_DEVICES=$(free-gpu) /home/aaror/yes/bin/python ./mnist_cuda.py \
-#        --epochs 4
+if [ $stage -le 1 ]; then
+  CUDA_VISIBLE_DEVICES=$(free-gpu) /home/aaror/yes/bin/python ./mnist_cuda.py \
+      --epochs 4
+  exit 0;
+fi
 
-#--gpu 1 --mem 2G limit_num_gpus.sh /home/aaror/yes/bin/python ./mnist_cuda.py \
-#          --name $name \
-#          --depth $depth \
-#          --batch-size $batch \
-#          --img-height $height \
-#          --img-width $width \
-#          --epochs $epochs
+if [ $stage -le 1 ]; then
+--gpu 1 --mem 2G limit_num_gpus.sh /home/aaror/yes/bin/python ./mnist_cuda.py \
+          --name $name \
+          --depth $depth \
+          --batch-size $batch \
+          --img-height $height \
+          --img-width $width \
+          --epochs $epochs
+fi
