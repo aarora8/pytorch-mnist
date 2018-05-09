8929e171a1f878dc06367f4a429783641682d001
aarora8 <aarora8@jhu.edu>
2018-05-09 15:38:24 -0400

    adding cmd.sh and queue.pl

--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+set -e # exit on error
+echo "$0 $@"
+. ./parse_options.sh
+
+stage=0
+
 /home/aaror/yes/bin/python ./mnist_cuda.py \
     --epochs 2 --no-cuda
 
 #CUDA_VISIBLE_DEVICES=$(free-gpu) /home/aaror/yes/bin/python ./mnist_cuda.py \
 #        --epochs 4
 
+#--gpu 1 --mem 2G limit_num_gpus.sh /home/aaror/yes/bin/python ./mnist_cuda.py \
+#          --name $name \
+#          --depth $depth \
+#          --batch-size $batch \
+#          --img-height $height \
+#          --img-width $width \
+#          --epochs $epochs
